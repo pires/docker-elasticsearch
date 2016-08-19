311414c927727af1d3609ff545231db7a88f12e5
Paulo Pires <pjpires@gmail.com>
2016-08-20 00:26:52 +0100

    Removed uneeded setup in runnable script.

--- run.sh
+++ run.sh
@@ -7,15 +7,6 @@ adduser elasticsearch sudo
 chown -R elasticsearch /elasticsearch /data
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
-# set environment
-export CLUSTER_NAME=${CLUSTER_NAME:-elasticsearch-default}
-export NODE_MASTER=${NODE_MASTER:-true}
-export NODE_DATA=${NODE_DATA:-true}
-export HTTP_ENABLE=${HTTP_ENABLE:-true}
-export HTTP_CORS_ENABLE=${HTTP_CORS_ENABLE:-true}
-export HTTP_CORS_ALLOW_ORIGIN=${HTTP_CORS_ALLOW_ORIGIN:-*}
-export NETWORK_HOST=${NETWORK_HOST:-_site_}
-
 # allow for memlock
 ulimit -l unlimited
 
