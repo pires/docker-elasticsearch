be6d09df37b9d96d17f9991442ca929916333dc9
Ed Robinson <ed.robinson@reevoo.com>
2016-11-08 20:07:33 +0000

    Replace sudo with su-exec
    It is simpler and does everything you would want here. It's also
    fractionaly smaller.

--- run.sh
+++ run.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # provision elasticsearch user
-addgroup sudo
 adduser -D -g '' elasticsearch
-adduser elasticsearch sudo
 chown -R elasticsearch /elasticsearch /data
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 # allow for memlock
 ulimit -l unlimited
 
 # run
-sudo -E -u elasticsearch /elasticsearch/bin/elasticsearch
+su-exec elasticsearch /elasticsearch/bin/elasticsearch
