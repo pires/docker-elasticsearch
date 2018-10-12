50c34a399d1ea21fd2b0d3a35edf48daba4a9ec5
Nikolay Kolev <nikolay@users.noreply.github.com>
2018-10-12 02:25:36 -0700

    Defensive Bash and more (#74)
    * Defensive Bash and more
    
    * Fix the typo

--- run.sh
+++ run.sh
@@ -1,75 +1,74 @@
 #!/bin/sh
 
-echo "Starting ElasticSearch $ES_VERSION"
+echo "Starting Elasticsearch ${ES_VERSION}"
 
 BASE=/elasticsearch
 
-# allow for memlock if enabled
-if [ "$MEMORY_LOCK" == "true" ]; then
+# Allow for memlock if enabled
+if [ "${MEMORY_LOCK}" == "true" ]; then
     ulimit -l unlimited
 fi
 
-# Set a random node name if not set.
+# Set a random node name if not set
 if [ -z "${NODE_NAME}" ]; then
-	NODE_NAME=$(uuidgen)
+    NODE_NAME="$(uuidgen)"
 fi
-export NODE_NAME=${NODE_NAME}
+export NODE_NAME="${NODE_NAME}"
 
-# Create a temporary folder for Elastic Search ourselves.
-# Ref: https://github.com/elastic/elasticsearch/pull/27659
-export ES_TMPDIR=`mktemp -d -t elasticsearch.XXXXXXXX`
+# Create a temporary folder for Elasticsearch ourselves
+# ref: https://github.com/elastic/elasticsearch/pull/27659
+export ES_TMPDIR="$(mktemp -d -t elasticsearch.XXXXXXXX)"
 
 # Prevent "Text file busy" errors
 sync
 
 if [ ! -z "${ES_PLUGINS_INSTALL}" ]; then
-   OLDIFS=$IFS
-   IFS=','
-   for plugin in ${ES_PLUGINS_INSTALL}; do
-      if ! $BASE/bin/elasticsearch-plugin list | grep -qs ${plugin}; then
-         until $BASE/bin/elasticsearch-plugin install --batch ${plugin}; do
-           echo "failed to install ${plugin}, retrying in 3s"
-           sleep 3
-         done
-      fi
-   done
-   IFS=$OLDIFS
+    OLDIFS="${IFS}"
+    IFS=","
+    for plugin in ${ES_PLUGINS_INSTALL}; do
+        if ! "${BASE}"/bin/elasticsearch-plugin list | grep -qs ${plugin}; then
+            until "${BASE}"/bin/elasticsearch-plugin install --batch ${plugin}; do
+                echo "Failed to install ${plugin}, retrying in 3s"
+                sleep 3
+            done
+        fi
+    done
+    IFS="${OLDIFS}"
 fi
 
 if [ ! -z "${SHARD_ALLOCATION_AWARENESS_ATTR}" ]; then
-    # this will map to a file like  /etc/hostname => /dockerhostname so reading that file will get the
-    #  container hostname
-    if [ "$NODE_DATA" == "true" ]; then
-        ES_SHARD_ATTR=`cat ${SHARD_ALLOCATION_AWARENESS_ATTR}`
+    # This will map to a file like  /etc/hostname => /dockerhostname so reading that file will get the
+    # container hostname
+    if [ "${NODE_DATA}" == "true" ]; then
+        ES_SHARD_ATTR="$(cat ${SHARD_ALLOCATION_AWARENESS_ATTR})"
         NODE_NAME="${ES_SHARD_ATTR}-${NODE_NAME}"
-        echo "node.attr.${SHARD_ALLOCATION_AWARENESS}: ${ES_SHARD_ATTR}" >> $BASE/config/elasticsearch.yml
+        echo "node.attr.${SHARD_ALLOCATION_AWARENESS}: ${ES_SHARD_ATTR}" >> "${BASE}"/config/elasticsearch.yml
     fi
     if [ "$NODE_MASTER" == "true" ]; then
-        echo "cluster.routing.allocation.awareness.attributes: ${SHARD_ALLOCATION_AWARENESS}" >> $BASE/config/elasticsearch.yml
+        echo "cluster.routing.allocation.awareness.attributes: ${SHARD_ALLOCATION_AWARENESS}" >> "${BASE}"/config/elasticsearch.yml
     fi
 fi
 
-# remove x-pack-ml module
-rm -rf /elasticsearch/modules/x-pack/x-pack-ml
-rm -rf /elasticsearch/modules/x-pack-ml
+# Remove x-pack-ml module
+rm -rf \
+    /elasticsearch/modules/x-pack/x-pack-ml \
+    /elasticsearch/modules/x-pack-ml
 
-# run
+# Run
 if [[ $(whoami) == "root" ]]; then
-    #chown -R elasticsearch:elasticsearch $BASE
-    #chown -R elasticsearch:elasticsearch /data
-	echo "Changing ownership of $BASE folder"
-	find . -type f -print0 | xargs -0 chown elasticsearch:elasticsearch $BASE
+    echo "Changing ownership of ${BASE} folder"
+    find "${BASE}" -type f -print0 | xargs -0 chown elasticsearch:elasticsearch
+
+    echo "Changing ownership of /data folder"
+    find /data -type f -print0 | xargs -0 chown elasticsearch:elasticsearch
 	
-	echo "Changing ownership of /data folder"
-	find . -type f -print0 | xargs -0 chown elasticsearch:elasticsearch /data
-	
-    exec su-exec elasticsearch $BASE/bin/elasticsearch $ES_EXTRA_ARGS
+    exec su-exec elasticsearch "${BASE}"/bin/elasticsearch ${ES_EXTRA_ARGS}
 else
-    # the container's first process is not running as 'root', 
-    # it does not have the rights to chown. however, we may
+    # The container's first process is not running as 'root', 
+    # it does not have the rights to chown. However, we may
     # assume that it is being ran as 'elasticsearch', and that
-    # the volumes already have the right permissions. this is
-    # the case for kubernetes for example, when 'runAsUser: 1000'
+    # the volumes already have the right permissions. This is
+    # the case for Kubernetes, for example, when 'runAsUser: 1000'
     # and 'fsGroup:100' are defined in the pod's security context.
-    $BASE/bin/elasticsearch $ES_EXTRA_ARGS
+    "${BASE}"/bin/elasticsearch ${ES_EXTRA_ARGS}
 fi
